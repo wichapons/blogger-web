714becc9ab09ea39529bbc18b4c5c714e9572230
Wichapon V <wichapon@gmail.com>
2023-02-21 20:42:58 +0700

    add function to view each post in full detail

--- routes/blog.js
+++ routes/blog.js
@@ -11,7 +11,7 @@ router.get('/',async (req,res)=>{
 
 router.get('/post',async (req,res)=>{
     const [postsDATA] = await db.query('SELECT posts.*,authors.name AS author_name FROM blog.posts INNER JOIN authors ON posts.author_id = authors.id;');
-    console.log(postsDATA);
+    //console.log(postsDATA[0].id);
     res.render('posts-list',{posts:postsDATA});
 });
 
@@ -23,12 +23,17 @@ router.post('/post',async (req,res)=>{
     formData.content,
     formData.author
 ]
-console.log(data);
 await db.query('INSERT INTO posts (title,summary,body,author_id) VALUES (?)',[data])
 res.redirect('/post')
 });
 
-
+router.get(('/view/:id'),async (req,res)=>{
+    const postID = req.params.id;
+    console.log(postID);
+    const [postBody] = await db.query(`SELECT * FROM posts WHERE id = ${postID}`);
+    console.log(postBody);
+    res.render('post-detail',{postDetails:postBody});
+})
 
 
 router.get('/new-post', async (req,res)=>{
