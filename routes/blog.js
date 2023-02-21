5bb2a8fb4f475ea21d32e7ff92f6317920fd542e
Wichapon V <wichapon@gmail.com>
2023-02-21 21:01:24 +0700

    add post number verification step in case that user manually type in the post number

--- routes/blog.js
+++ routes/blog.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const db = require('../database/database.js')
 
 
-
-
 router.get('/',async (req,res)=>{
     res.redirect('/post');
 });
@@ -30,11 +28,16 @@ res.redirect('/post')
 router.get(('/view/:id'),async (req,res)=>{
     const postID = req.params.id;
     console.log(postID);
-    const [postBody] = await db.query(`SELECT * FROM posts WHERE id = ${postID}`);
+    const [postBody] = await db.query(`SELECT *  FROM posts INNER JOIN authors ON posts.author_id = authors.id WHERE posts.id = ${postID}`);
     console.log(postBody);
-    res.render('post-detail',{postDetails:postBody});
+    if (!postBody || postBody.length===0){  //in case that user manually type in the post number and it's not found on our server
+        console.log('hi');
+        res.render('404')
+    }else{
+        res.render('post-detail',{postDetails:postBody});
+    }
+    
 })
-
 
 router.get('/new-post', async (req,res)=>{
     const [authorData] = await db.query('SELECT * FROM authors');
